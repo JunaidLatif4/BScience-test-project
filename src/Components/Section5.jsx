a24f0617a27af449d5592275ba131008df0eff74
Juniad Latif <junaidlatif004@gmail.com>
2021-07-02 17:42:40 +0500

    Section 4 Added

---
+++ src/Components/Section5.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './CSS/Section4.scss'
+const Section5 = () => {
+    return (
+        <>
+            <div className="section5_container">
+
+            </div>
+        </>
+    )
+}
+
+export default Section5
