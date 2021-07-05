473ff21a12e38fbcd28a9cd3743bd3528fdcda63
Juniad Latif <junaidlatif004@gmail.com>
2021-07-05 15:25:20 +0500

    Mobile Responsive Added

---
+++ src/Components/Section5CardsMbl.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Section5CardsMbl = () => {
+    return (
+        <>
+        <div className="section5cardsmbl_container">
+            
+        </div>
+        </>
+    )
+}
+
+export default Section5CardsMbl
