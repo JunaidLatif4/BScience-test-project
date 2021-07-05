ac7fa9c07a059c3eca3d9da55be35d3d86e53e96
Juniad Latif <junaidlatif004@gmail.com>
2021-07-05 12:06:52 +0500

    Footer Added

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Section3 from './Components/Section3'
 import Section4 from './Components/Section4'
 import Section5 from './Components/Section5'
 import Section6 from './Components/Section6'
+import Footer from './Components/Footer'
 
 import './App.scss'
 
@@ -19,6 +20,8 @@ const App = () => {
       <Section4 />
       <Section5 />
       <Section6 />
+      <Footer/>
+      
     </>
   )
 }
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+import GitHubIcon from '@material-ui/icons/GitHub';
+import TelegramIcon from '@material-ui/icons/Telegram';
+import MailIcon from '@material-ui/icons/Mail';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import { makeStyles } from '@material-ui/core'
+
+import './CSS/Footer.scss'
+
+const Styles = makeStyles({
+    btn: {
+        backgroundColor: "#323641",
+        color: "#8c9eff",
+        padding: ".5rem",
+        fontSize: "1.5rem",
+        borderRadius: "5rem",
+        marginRight: "1rem"
+
+    }
+})
+
+const Section7 = () => {
+    const classes = Styles();
+    return (
+        <>
+            <div className="footer_container">
+                <div className="data">
+                    <div className="icons">
+                        <GitHubIcon className={classes.btn}/>
+                        <TelegramIcon className={classes.btn}/>
+                        <MailIcon className={classes.btn}/>
+                        <TwitterIcon className={classes.btn}/>
+                    </div>
+                    <hr />
+                    <p> © Copyright Bscience</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Section7
