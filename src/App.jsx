cf5a85abbc36a4d416f7de60d8500f2f669732bf
Juniad Latif <junaidlatif004@gmail.com>
2021-07-05 10:05:50 +0500

    Section 5 Added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Section1 from './Components/Section1'
 import Section2 from './Components/Section2'
 import Section3 from './Components/Section3'
 import Section4 from './Components/Section4'
+import Section5 from './Components/Section5'
 
 import './App.scss'
 
@@ -15,6 +16,7 @@ const App = () => {
       <Section2 />
       <Section3 />
       <Section4 />
+      <Section5 />
     </>
   )
 }
--- src/Components/Section5.jsx
+++ src/Components/Section5.jsx
@@ -1,11 +1,62 @@
 import React from 'react'
 
-import './CSS/Section4.scss'
+import BackImg7 from '../imgs/background7.svg'
+import './CSS/Section5.scss'
+
 const Section5 = () => {
     return (
         <>
             <div className="section5_container">
-
+                <img src={BackImg7} alt="ERROR" className="back" />
+                <div className="heading">
+                    <h1> Road Map </h1> <hr />
+                </div>
+                <div className="data">
+                    <div className="card">
+                        <h1>Heading</h1>
+                        <p className="content">YFScience is a new generation decentralised platform that enables different features for staking, lending, trading and coinvesting. YFSI is the governance token of YFScience that enables access to an on-chain decision making model..</p>
+                        <div className="line">
+                            <div className="year">2020</div>
+                            <p>Q1</p>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="line">
+                            <div className="year">2020</div>
+                            <div className="border_down"></div>
+                            <p>Q1</p>
+                        </div>
+                        <h1>Heading</h1>
+                        <p className="content">YFScience is a new generation decentralised platform that enables different features for staking, lending, trading and coinvesting. YFSI is the governance token of YFScience that enables access to an on-chain decision making model..</p>
+                    </div>
+                    <div className="card">
+                        <h1>Heading</h1>
+                        <p className="content">YFScience is a new generation decentralised platform that enables different features for staking, lending, trading and coinvesting. YFSI is the governance token of YFScience that enables access to an on-chain decision making model..</p>
+                        <div className="line">
+                            <div className="year">2020</div>
+                            <div className="border_up"></div>
+                            <p>Q1</p>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="line">
+                            <div className="year">2020</div>
+                            <div className="border_down"></div>
+                            <p>Q1</p>
+                        </div>
+                        <h1>Heading</h1>
+                        <p className="content">YFScience is a new generation decentralised platform that enables different features for staking, lending, trading and coinvesting. YFSI is the governance token of YFScience that enables access to an on-chain decision making model..</p>
+                    </div>
+                    <div className="card">
+                        <h1>Heading</h1>
+                        <p className="content">YFScience is a new generation decentralised platform that enables different features for staking, lending, trading and coinvesting. YFSI is the governance token of YFScience that enables access to an on-chain decision making model..</p>
+                        <div className="line">
+                            <div className="year">2020</div>
+                            <div className="border_up"></div>
+                            <p>Q1</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
